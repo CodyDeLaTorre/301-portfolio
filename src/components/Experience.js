52bd97c036c2872910cca7137757ed3a9beac58f
CODY DE LA TORRE <cdelatorre414@gmail.com>
2022-11-11 16:27:05 -0800

    added background image and css

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -66,8 +66,8 @@ class Experience extends Component {
       <section id="resume" className="pb-5">
         <div className="col-md-12 mx-auto">
           <div className="col-md-12">
-            <h1 className="section-title" style={{ color: "black" }}>
-              <span className="text-black" style={{ textAlign: "center" }}>
+            <h1 className="section-title" style={{ color: "#F27405" }}>
+              <span className="wantChange" style={{ textAlign: "center", color: "#F27405" }}>
                 {sectionName}
               </span>
             </h1>
